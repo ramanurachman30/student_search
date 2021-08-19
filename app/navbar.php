5c15433970e7aeb86d6e0fd277ac2cbe51c3c73c
Muhammad Nurachman Musyaffa <63632172+ramanurachman30@users.noreply.github.com>
2021-08-19 10:36:21 +0700

    Add files via upload

---
+++ app/navbar.php
@@ -0,0 +1,12 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top">
+    <div class="container-fluid">
+        <!-- Offcanvas button -->
+        <button class="navbar-toggler me-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">
+            <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
+        </button>
+        <!-- End Offcanvas button-->
+        <a class="navbar-brand fw-bold text-uppercase me-auto" href="#">Student Search</a>
+    </div>
+</nav>
+<!-- End Navbar -->
---
+++ app/sidebar.php
@@ -0,0 +1,27 @@
+<!-- Offcanvas -->
+<div class="offcanvas offcanvas-start sidebar-nav text-white bg-dark" tabindex="-1" id="sidebar" aria-labelledby="sidebar">
+    <div class="offcanvas-body p-0">
+      <nav class="navbar-dark">
+          <ul class="navbar-nav">
+              <li>
+                  <hr class="dropdown-divider">
+              </li>
+              <li>
+                  <div class="text-muted small fw-bold text-uppercase px-3">
+                  DATA
+                  </div>
+              </li>
+              <li>
+                  <a href="" class="nav-link px-3">
+                      <span><i class='bx bxs-layout'></i></span>
+                      <span class="px-2">Siswa</span>
+                  </a>
+              </li>
+              <li>
+                  <hr class="dropdown-divider">
+              </li>
+              </ul>
+        </nav>
+    </div>
+</div>
+<!-- End Offcanvas -->
